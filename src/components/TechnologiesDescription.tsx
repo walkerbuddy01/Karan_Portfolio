800ea32f8b8a4e85a996fb4e6265d87071584e7b
Karan Sharma <karansharma40692@gmail.com>
2024-11-22 10:28:52 +0530

    Nothing Updated

--- src/components/TechnologiesDescription.tsx
+++ src/components/TechnologiesDescription.tsx
@@ -5,7 +5,7 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);  
 
 function TechnologiesDescription() {
   const scrollTriggerHolder = useRef<HTMLDivElement>(null);
